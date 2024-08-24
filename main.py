bf0d3cd0c9dd032785a8c5478770bb80076f8f96
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-08-24 11:00:56 -0500

    Finished base functionality

---
+++ main.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import ttk
+
+def convert():
+    mile_input = entry_int.get()
+    km_output = mile_input * 1.61
+    output_string.set(km_output)
+
+#window
+window = tk.Tk()
+window.title('Miles to Kilometers')
+window.geometry('350x150')
+
+#title
+title_label = ttk.Label(master = window, text = 'Miles to Kilometers', font = 'Courier 20 bold')
+title_label.pack(pady = 5)
+
+#input field
+input_frame = ttk.Frame(master = window)
+entry_int = tk.IntVar()
+entry = ttk.Entry(master = input_frame, textvariable = entry_int)
+button = ttk.Button(master = input_frame, text='Convert', command = convert)
+entry.pack(side = 'left', padx = 10)
+button.pack(side = 'left')
+input_frame.pack(pady = 15)
+
+#output
+output_string = tk.StringVar()
+output_label = ttk.Label(
+    master = window,
+    text = 'Kilometers',
+    font = 'Courier 20',
+    textvariable = output_string)
+output_label.pack(pady = 5)
+
+#run
+window.mainloop()
